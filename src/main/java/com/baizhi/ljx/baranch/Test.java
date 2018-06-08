9a8329235b770e7fc19871a2ab704e36fba224e1
ljx <985126361@qq.com>
2018-06-08 20:20:23 +0800

    分支第1次提交

---
+++ src/main/java/com/baizhi/ljx/baranch/Test.java
@@ -0,0 +1,11 @@
+package com.baizhi.ljx.baranch;
+
+/**
+ * Created by Administrator on 2018/6/8.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("我是分支");
+    }
+}
